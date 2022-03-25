82131ec615dd5913f5e83c5a334903f348b59acc
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-24 22:50:43 -0300

    :wrench: chore: database settings

---
+++ database/session.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+from core.config import settings
+
+
+engine = create_engine(settings.DATABASE_URI, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base = declarative_base()
