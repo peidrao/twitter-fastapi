41bcb586d3472482b4dccb1a5b6806801125e930
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-24 22:45:22 -0300

    :wrench: chore: settings file

---
+++ core/config.py
@@ -0,0 +1,14 @@
+import secrets
+
+from pydantic.class_validators import validator
+from pydantic import BaseSettings
+
+
+
+class Settings(BaseSettings):
+    API_V1: str
+    SECRET_KEY: secrets.token_urlsafe(32)
+    PROJECT_NAME: str
+
+
+settings = Settings()
