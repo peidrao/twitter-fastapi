4e0ab2a7326fc7e2901012a1834ee17cc45c9ca5
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-26 11:28:38 -0300

    :sparkles: feat: update url main

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from core.config import settings
 from database.session import Base, engine
 
 
-Base.metadata.create_all(bind=engine)
-app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f'{settings.API_V1}/openapi.json')
+from routers.routers import api_router
 
+Base.metadata.create_all(bind=engine)
+# app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f'{settings.API_V1}/openapi.json')
+app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f'/{settings.API_V1}/openapi.json')
+
+app.include_router(api_router)
