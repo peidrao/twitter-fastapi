e3fd746d1b152557df2c0f9b39b9c71173512826
Pedro Fonseca <contatopedrorn@gmail.com>
2022-03-26 09:56:25 -0300

    :sparkles: feat: create user

---
+++ services/user.py
@@ -0,0 +1,27 @@
+from typing import List, Optional
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+
+from database.session import Base
+from models.user import User
+from utils.hash import Hash
+from schemas.user import UserBase, UserDisplay
+
+
+class UserService(Base[User, UserBase]):
+    def create(self, db: Session, request: UserBase) -> User:
+        user = User(
+            username=request.username,
+            email=request.email,
+            password=Hash.bcrypt(request.password)
+        )
+
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        return user
+
+
+
+user = UserService(User)
