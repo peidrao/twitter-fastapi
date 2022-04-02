0dcf94255de1de16a72fbfa9aeb1c671f9442ffb
Pedro Fonseca <pedro.fonseca@delinea.com.br>
2022-04-01 22:29:29 -0300

    :sparkles: - feat: session meta

--- database/session.py
+++ database/session.py
@@ -1,12 +1,13 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy import MetaData
 
 from core.config import settings
 
 
-engine = create_engine(settings.DATABASE_URI, connect_args={"check_same_thread": False})
+engine = create_engine(settings.DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
+# meta = MetaData()
+# connection = engine.connect()
 Base = declarative_base()
