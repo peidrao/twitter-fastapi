42f4db1ff307f35d7272679557bf6d84cb496bc9
Pedro Fonseca <pedro.fonseca@delinea.com.br>
2022-04-03 11:12:31 -0300

    :recycle: refactor - update user routers

--- routers/user.py
+++ routers/user.py
@@ -1,25 +1,29 @@
 from typing import Any, List
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
+from sqlmodel import Session
 
 from schemas.user import UserDisplay, UserBase
 from services.user import user
-# from database.db import get_db
+from database.session import engine
 
 
 router = APIRouter()
 
 
-# @router.post('/', response_model=UserDisplay)
-# async def create_user(request: UserBase, db: Session = Depends(get_db)) -> Any:
-#     object = user.create(db=db, request=request)
-#     return object
+@router.post('/', response_model=UserDisplay)
+async def create_user(request: UserBase) -> Any:
+    with Session(engine) as session:
+        
+        object = user.create(db=session, request=request)
+        return object
 
-# @router.delete('/{id}', response_model=None)
-# async def delete(db: Session = Depends(get_db), id=id) -> Any:
-#     object = user.delete(db=db, id=id)
-#     return object
+@router.delete('/{id}', response_model=None)
+async def delete(id=id) -> Any:
+    with Session(engine) as session:
+        object = user.delete(db=session, id=id)
+        return object
 
-# @router.get('/', response_model=List[UserDisplay])
-# async def get_users(db: Session = Depends(get_db)) -> Any:
-#     return user.get_all(db=db)
+@router.get('/', response_model=List[UserDisplay])
+async def get_users() -> Any:
+    with Session(engine) as session:
+        return user.get_all(db=session)
