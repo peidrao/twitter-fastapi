5ed2a1e2e3a2c743882fb10b5973398649283aac
Pedro Fonseca <contatopedrorn@gmail.com>
2022-04-03 17:08:00 -0300

    :sparkles: feat - tweet model

--- models/tweet.py
+++ models/tweet.py
@@ -1,34 +1,20 @@
-from turtle import back
+from datetime import date, datetime
+from typing import Optional
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlmodel import Field, SQLModel
 from sqlalchemy.orm import relationship
 
-from .user import User
-from database.session import Base
+# from .user import User
+# from database.session import Base
 
 
-# class Tweet(Base):
-#     __tablename__ = 'tweet'
-#     id = Column(Integer, primary_key=True, index=True)
+class Tweet(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    text: str
 
-#     is_active = Column(Boolean, default=True)
-#     created_at = Column(DateTime)
-#     deleted_at = Column(DateTime, default=None)
+    is_active: Optional[bool] = Field(default=True)
+    created_at: Optional[datetime] = Field(default=datetime.utcnow)
+    updated_at: Optional[datetime] = Field(default=datetime.utcnow)
     
-#     user_id = Column(Integer, ForeignKey('user.id'))
-#     user = relationship(User, back_populates='items')
+    user: Optional[int] = Field(default=None, foreign_key="user.id")
 
-
-
-# class User(SQLModel, table=True):
-#     id: Optional[int] = Field(default=None, primary_key=True)
-#     name: str
-#     username: Optional[str] = Field(max_length=120, nullable=False)
-#     email: Optional[str] = Field(max_length=200, nullable=False)
-#     password: Optional[str] = Field(max_length=200, nullable=False)
-#     birthday: Optional[datetime] = Field(nullable=False)
-
-#     is_active: Optional[bool] = Field(default=True)
-#     is_banned: Optional[bool] = Field(default=False)
-
-#     created_at: Optional[datetime] = Field(default=datetime.utcnow)
-#     updated_at: Optional[datetime] = Field(default=datetime.utcnow)
