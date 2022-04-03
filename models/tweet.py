d05ce99cbb89021f24b3babd907f4ebd63b9ec93
Pedro Fonseca <contatopedrorn@gmail.com>
2022-04-03 17:08:41 -0300

    :recycle: refactor - remove libraries

--- models/tweet.py
+++ models/tweet.py
@@ -1,11 +1,6 @@
-from datetime import date, datetime
+from datetime import datetime
 from typing import Optional
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 from sqlmodel import Field, SQLModel
-from sqlalchemy.orm import relationship
-
-# from .user import User
-# from database.session import Base
 
 
 class Tweet(SQLModel, table=True):
@@ -17,4 +12,3 @@ class Tweet(SQLModel, table=True):
     updated_at: Optional[datetime] = Field(default=datetime.utcnow)
     
     user: Optional[int] = Field(default=None, foreign_key="user.id")
-
