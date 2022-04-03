a00ecdb01e906f396d643889223c7b974624c863
Pedro Fonseca <pedro.fonseca@delinea.com.br>
2022-04-03 11:12:53 -0300

    :recycle: refactor - remove comments app

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 
 from core.config import settings
-# from database.session import Base, engine
 from database.session import create_db
-
 from routers.routers import api_router
 
-# Base.metadata.create_all(bind=engine)
-# app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f'{settings.API_V1}/openapi.json')
+
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f'{settings.API_V1}/openapi.json')
 
 @app.on_event("startup")
