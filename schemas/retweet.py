76908a99705cc16540be4804209a553f3f0e2c17
Pedro Fonseca <contatopedrorn@gmail.com>
2022-04-09 11:38:19 -0300

    :sparkles: feat - add retweet schema

--- schemas/retweet.py
+++ schemas/retweet.py
@@ -5,3 +5,12 @@ from typing import Optional
 class RetweetBase(BaseModel):
     tweet: int
     comment: Optional[str]
+
+
+class RetweetDisplay(BaseModel):
+    tweet: int
+    username: str
+    comment: Optional[str]
+
+    class Config:
+        orm_mode = True
