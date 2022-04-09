6bee63bb0978c713f693a1fbee642abc54381c90
Pedro Fonseca <contatopedrorn@gmail.com>
2022-04-09 10:51:19 -0300

    :sparkles: feat - Retweet router

---
+++ routers/retweet.py
@@ -0,0 +1,19 @@
+from typing import Any
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+from authentication.oauth import get_current_user
+
+from schemas.user import UserAuth
+from schemas.retweet import RetweetBase
+from services.retweet import retweet
+from database.session import engine
+
+
+router = APIRouter()
+
+
+@router.post('/', response_model=None)
+async def create_retweet(request: RetweetBase, request_user: UserAuth = Depends(get_current_user)) -> Any:
+    with Session(engine) as session:
+        object = retweet.create(db=session, request=request, request_user=request_user)
+        return object
