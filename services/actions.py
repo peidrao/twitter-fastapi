ec8d0cbd425b414d226e8cc1fb3f4f971771f2e9
Pedro Fonseca <contatopedrorn@gmail.com>
2022-04-10 10:13:49 -0300

    :bug: fix - update create action

--- services/actions.py
+++ services/actions.py
@@ -1,28 +1,24 @@
-from fastapi import  HTTPException
-
 from sqlmodel import Session
 
 from models import UserAction
 from schemas.user import UserAuth
 from schemas.actions import UserActionBase
 
-
 from services.user import user as user_service
-from utils.tweet_addons import get_tweet_actions
 
 
 class ActionService:
     def create(self, db: Session, request: UserActionBase, request_user: UserAuth) -> UserAction:
         user = user_service.get_user_by_id(db, request_user['id'])
         
-        action = db.query(UserAction).filter(UserAction.user == request_user['id']).first()
+        action = db.query(UserAction).filter(UserAction.user == request_user['id'], UserAction.user_ref == request.user_ref).first()
 
         if action:
-            if request['is_blocked']:
+            if request.is_blocked:
                 action.is_blocked = True
-            elif request['is_followed']:     
+            elif request.is_followed:     
                 action.is_followed = True
-            elif request['is_muted']:
+            elif request.is_muted:
                 action.is_muted = True
         else:
             action  = UserAction(user_ref=request.user_ref, user=user.id, is_blocked=request.is_blocked,
@@ -32,7 +28,6 @@ class ActionService:
         db.commit()
         db.refresh(action)
         return action
-
 
 
 action = ActionService()
