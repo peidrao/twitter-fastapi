93292a73c96832d35c0af99fd86ca967ce42fc6b
Pedro Fonseca <contatopedrorn@gmail.com>
2022-04-09 21:06:51 -0300

    :sparkles: feat - user actions service

---
+++ services/actions.py
@@ -0,0 +1,38 @@
+from fastapi import  HTTPException
+
+from sqlmodel import Session
+
+from models import UserAction
+from schemas.user import UserAuth
+from schemas.actions import UserActionBase
+
+
+from services.user import user as user_service
+from utils.tweet_addons import get_tweet_actions
+
+
+class ActionService:
+    def create(self, db: Session, request: UserActionBase, request_user: UserAuth) -> UserAction:
+        user = user_service.get_user_by_id(db, request_user['id'])
+        
+        action = db.query(UserAction).filter(UserAction.user == request_user['id']).first()
+
+        if action:
+            if request['is_blocked']:
+                action.is_blocked = True
+            elif request['is_followed']:     
+                action.is_followed = True
+            elif request['is_muted']:
+                action.is_muted = True
+        else:
+            action  = UserAction(user_ref=request.user_ref, user=user.id, is_blocked=request.is_blocked,
+                        is_muted=request.is_muted, is_followed=request.is_followed)
+            db.add(action)
+        
+        db.commit()
+        db.refresh(action)
+        return action
+
+
+
+action = ActionService()
