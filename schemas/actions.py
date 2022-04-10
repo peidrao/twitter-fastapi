eae418e85c2fccaaf6284583bcbaba61a29ba2f7
Pedro Fonseca <contatopedrorn@gmail.com>
2022-04-10 11:41:38 -0300

    :truck: refactor -  rename schema action

--- schemas/actions.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-
-class UserActionBase(BaseModel):
-    user_ref: int
-    is_blocked: Optional[bool]
-    is_muted: Optional[bool]
-    is_followed: Optional[bool]
