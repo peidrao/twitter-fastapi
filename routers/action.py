68cf56138e68c00f2ebf8a3bb3dcb35901e78066
Pedro Fonseca <contatopedrorn@gmail.com>
2022-04-09 21:06:41 -0300

    :sparkles: feat - user actions routers

---
+++ routers/action.py
@@ -0,0 +1,18 @@
+from typing import Any
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+from authentication.oauth import get_current_user
+
+from schemas.user import UserAuth
+from schemas.actions import UserActionBase
+from services.actions import action
+from database.session import engine
+
+
+router = APIRouter()
+
+@router.post('/follow/')
+async def follow(request: UserActionBase, request_user: UserAuth = Depends(get_current_user)) -> Any:
+    with Session(engine) as session:
+        
+        return action.create(db=session, request=request, request_user=request_user)
--- routers/routers.py
+++ routers/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from routers import user, authentication, tweet, like, retweet
+from routers import action, user, authentication, tweet, like, retweet
 
 
 api_router = APIRouter()
@@ -9,3 +9,4 @@ api_router.include_router(tweet.router, prefix='/tweets', tags=['tweet'])
 api_router.include_router(authentication.router, prefix='/login', tags=['login'])
 api_router.include_router(like.router, prefix='/likes', tags=['like'])
 api_router.include_router(retweet.router, prefix='/retweets', tags=['retweet'])
+api_router.include_router(action.router, prefix='/actions', tags=['action'])
