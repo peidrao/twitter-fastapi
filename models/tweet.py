de4266a90ae715608b5c471668f1af8af6707416
Pedro Fonseca <contatopedrorn@gmail.com>
2022-08-31 22:10:39 -0300

    update tables

--- models/tweet.py
+++ models/tweet.py
@@ -4,6 +4,7 @@ from sqlmodel import Field, SQLModel
 
 
 class Tweet(SQLModel, table=True):
+    __tablename__ = 'tweets'
     id: Optional[int] = Field(default=None, primary_key=True)
     text: str
     likes: Optional[int]
@@ -11,19 +12,20 @@ class Tweet(SQLModel, table=True):
     created_at: Optional[datetime] = Field(default=datetime.utcnow())
     updated_at: Optional[datetime] = Field(default=datetime.utcnow())
     
-    user: Optional[int] = Field(default=None, foreign_key="user.id")
+    user: Optional[int] = Field(default=None, foreign_key="users.id")
 
 
 class Like(SQLModel, table=True):
+    __tablename__ = 'likes'
     id: int = Field(default=None, primary_key=True)
-    tweet: Optional[int] = Field(default=None, foreign_key="tweet.id")
-    user: Optional[int] = Field(default=None, foreign_key="user.id")
+    tweet: Optional[int] = Field(default=None, foreign_key="tweets.id")
+    user: Optional[int] = Field(default=None, foreign_key="users.id")
     is_active: Optional[bool] = Field(default=True)
 
 
 class Retweet(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     comment: Optional[str] = Field(max_length=280)
-    tweet: Optional[int] = Field(default=None, foreign_key="tweet.id")
-    user: Optional[int] = Field(default=None, foreign_key="user.id")
+    tweet: Optional[int] = Field(default=None, foreign_key="tweets.id")
+    user: Optional[int] = Field(default=None, foreign_key="users.id")
     is_active: Optional[bool] = Field(default=True)
