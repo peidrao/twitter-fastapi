9c5a8f8643318fee9b040caec4f092cb8bf5ba5a
Pedro Fonseca <contatopedrorn@gmail.com>
2022-09-14 20:53:42 -0300

    add sqlalchemy in user and follow models

--- models/user.py
+++ models/user.py
@@ -1,29 +1,35 @@
 from datetime import date, datetime
 from typing import Optional
-from sqlmodel import Field, SQLModel
+from sqlalchemy import Boolean, ForeignKey, Integer, String, Column, DateTime, Date
+from core.database import Base
+from sqlalchemy.orm import relationship
 
 
-class User(SQLModel, table=True):
+class User(Base):
     __tablename__ = 'users'
-    id: int = Field(default=None, primary_key=True)
-    name: str = Field(max_length=120, nullable=False)
-    username: str = Field(max_length=120, nullable=False)
-    email: str = Field(max_length=200, nullable=False)
-    password: str = Field(max_length=200, nullable=False)
-    birthday: Optional[date] = Field(nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, max_length=120, nullable=False)
+    username = Column(String, max_length=120, unique=True, nullable=False)
+    email = Column(String, max_length=200, nullable=False)
+    password= Column(String, max_length=200, nullable=False)
+    birthday = Column(Date, nullable=True)
 
-    is_active: Optional[bool] = Field(default=True)
-    is_banned: Optional[bool] = Field(default=False)
+    is_active = Column(Boolean, default=True)
+    is_banned = Column(Boolean, default=False)
 
-    created_at: datetime = Field(default=datetime.utcnow())
-    updated_at: Optional[datetime] = Field(default=datetime.utcnow())
+    created_at= Column(DateTime, default=datetime.utcnow())
+    updated_at =  Column(DateTime, default=datetime.utcnow())
 
 
-class Follow(SQLModel, table=True):
+class Follow(Base):
     __tablename__ = 'follows'
-    id: int = Field(default=None, primary_key=True)
-    user: Optional[int] = Field(default=None, foreign_key="users.id")
-    user_ref: Optional[int] = Field(default=None, foreign_key="users.id")
-    is_followed: Optional[bool] = Field(default=False)
-    is_muted: Optional[bool] = Field(default=False)
-    is_blocked: Optional[bool] = Field(default=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user_ref_id = Column(Integer, ForeignKey("users.id"))
+    is_followed = Column(Boolean, default=False)
+    is_muted = Column(Boolean,  default=False)
+    is_blocked = Column(Boolean, default=False)
+    
+    user = relationship(User, back_populates="follows")
+    user_ref = relationship(User, back_populates="follows")
+
