94ddb60dfa5c36aeaf7b71ce9dd340ddaad386eb
Pedro Fonseca <contatopedrorn@gmail.com>
2022-09-18 12:00:41 -0300

    expire on commit

--- database/session.py
+++ database/session.py
@@ -4,4 +4,4 @@ from core.config import settings
 
 engine = create_engine(settings.SQLAlCHEMY_DATABASE_URI)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
