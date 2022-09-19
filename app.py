1802534aec44f74143ac6e277f1eb6f315a3ef74
Pedro Fonseca <contatopedrorn@gmail.com>
2022-09-19 20:22:48 -0300

    fix: create database

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from core.config import settings
 from routers.routers import api_router
+from database import engine, Base
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=settings.OPEN_API)
+
+@app.on_event("startup")
+async def startup_database():
+    logger.info("creating initial data")
+    Base.metadata.create_all(bind=engine)
+    logger.info("Initial data created")
+    
+
 
 app.include_router(api_router)
