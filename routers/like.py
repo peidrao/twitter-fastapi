bae28c08889f3f98bd2b7b01d2fb14aaefef72b0
Pedro Fonseca <contatopedrorn@gmail.com>
2022-09-23 20:01:28 -0300

    create schema like response

--- routers/like.py
+++ routers/like.py
@@ -1,9 +1,8 @@
 from typing import Any
 from fastapi import APIRouter, Depends
-from sqlmodel import Session
+
 from authentication.oauth import get_current_user
-from schemas.tweet import LikeBase
-from database import SessionLocal
+from schemas.like import LikeResponse
 from schemas.user import UserAuth
 from services.like import like
 
@@ -11,6 +10,6 @@ from services.like import like
 router = APIRouter()
 
 
-@router.post('/{tweet_id}', response_model=None)
+@router.post('/{tweet_id}', response_model=LikeResponse)
 async def create_like(tweet_id: int, request_user: UserAuth = Depends(get_current_user)) -> Any:
     return like.create(tweet_id, request_user=request_user)
